fe6070d932d4eaec2cac96cfe44c490c11ae1b0d
daseul22 <tlaektmf27@gmail.com>
2020-10-07 18:41:30 +0900

    Init

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import ReduxThunk from "redux-thunk";
+import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import logger from "redux-logger";
+//import { deprecated, ActionType, createReducer } from 'typesafe-actions'; 최신버전
+//const { createAction, createStandardAction } = deprecated;
+
+const customHistory = createBrowserHistory();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      logger
+    )
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router history={customHistory}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
