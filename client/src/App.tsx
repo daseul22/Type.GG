8279a6d19debfa9a24008d6b132fee0622700877
daseul22 <tlaektmf27@gmail.com>
2020-10-10 22:11:26 +0900

    Add page routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+
+import HomePage from "./page/HomePage";
+import ChampionPage from "./page/ChampionPage";
+import RankingPage from "./page/RankingPage";
+import LoginPage from "./page/LoginPage";
+import CommunityPage from "./page/CommunityPage";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <ul>Hello</ul>
-      </div>
+    <div className="wrap">
+      <Header />
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/champion" component={ChampionPage} />
+        <Route path="/ranking" component={RankingPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/community" component={CommunityPage} />
+      </Switch>
     </div>
   );
 }
