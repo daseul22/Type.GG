29628dd2f127a41a57656126b90f4e80f86fe3b1
daseul22 <tlaektmf27@gmail.com>
2020-10-10 22:13:38 +0900

    Add menu components

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Menu.css";
+import { Link } from "react-router-dom";
+
+interface MenuItem {
+  name: string;
+  link: string;
+}
+
+function Menu() {
+  const menu: MenuItem[] = [
+    { name: "#집에있자", link: "/" },
+    { name: "챔피언 분석", link: "/champion" },
+    { name: "랭킹", link: "/ranking" },
+    { name: "커뮤니티", link: "/community" },
+  ];
+  return (
+    <div className="l-menu">
+      <ul className="menu">
+        {menu.map(({ name, link }, id) => {
+          return (
+            <li key={id} className="menu_item">
+              <Link className="target" to={link}>
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
