9ef4b322414b26b45849638a79068f0a9bc27eda
daseul22 <tlaektmf27@gmail.com>
2020-10-16 14:42:12 +0900

    Feat HomeLogo

---
+++ client/src/components/HomeLogo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HomeLogo() {
+  return (
+    <div className="index-logo">
+      <div id="logo">
+        <img
+          src="https://placehold.it/332x200"
+          title="진"
+          alt="TP.GG logo ()"
+        />
+      </div>
+    </div>
+  );
+}
+export default HomeLogo;
